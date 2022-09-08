0f7f6f7bc977896a411845f14b56ae88da7ccffb
Alexander <haktoid@gmail.com>
2022-09-09 00:29:24 +0300

    My first attempt at uploading it somehow. I hope it's successful.

---
+++ PZ_1_RandomNumbers/PZ_1_RandomNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PZ_1_RandomNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var rnd = new Random(1234);
+            for (int i = 5; i < 10; i++)
+            {
+                var nrnd = rnd.Next(0, 10);
+                Console.WriteLine(nrnd);
+            }
+        }
+    }
+}
