3271cfb6a09b30dc6717192075ab631f36722c57
Alexander <haktoid@gmail.com>
2022-09-14 10:02:47 +0300

    Slightly changed Звіт, creating a folder for the second laboratory

--- PZ_1_RandomNumbers/PZ_1_RandomNumbers-2/Program.cs
+++ PZ_1_RandomNumbers/PZ_1_RandomNumbers-2/Program.cs
@@ -5,7 +5,7 @@ namespace PZ_1_RandomNumbers_2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()//string[] args)
         {
             for (int i = 0; i < 10; i++)
             {
---
+++ PZ_2_VernamCipher/PZ_2_VernamCipher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PZ_2_VernamCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
