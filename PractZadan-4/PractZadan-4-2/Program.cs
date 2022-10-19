5aafcaae34b2d03468a6d18268bdca34edc617ed
Alexander <haktoid@gmail.com>
2022-10-19 15:06:12 +0300

    Lab4 (task 3 and 4)

---
+++ PractZadan-4/PractZadan-4-2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.Diagnostics;
+
+namespace PractZadan_4_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Choose: MD5, SHA1, SHA256, SHA384 or SHA512");
+            string choice = Console.ReadLine();
+            const string passwordToHash = "VeryComplexPassword";
+            int count = 0;
+            int iter = 230000;
+            while (count!=10)
+            { 
+               HashPassword(passwordToHash, iter, choice);
+               iter += 50000;
+               count += 1;
+            }
+            Console.ReadLine();
+        }
+        private static void HashPassword(string passwordToHash, int numberOfRounds, string choice)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var hashedPassword = PBKDF2.HashPassword(Encoding.UTF8.GetBytes(passwordToHash),PBKDF2.GenerateSalt(),numberOfRounds, choice);
+            sw.Stop();
+            Console.WriteLine();
+            Console.WriteLine("Password to hash : " + passwordToHash);
+            Console.WriteLine("Hashed Password : " +  Convert.ToBase64String(hashedPassword));
+            Console.WriteLine("Iterations <" + numberOfRounds + ">  Elapsed Time: " +  sw.ElapsedMilliseconds + "ms");
+        }
+    }
+    public class PBKDF2
+    {
+        public static byte[] GenerateSalt()
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[32];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+        public static byte[] HashPassword(byte[] toBeHashed, byte[] salt, int numberOfRounds, string choice)
+        {
+            if (choice == "MD5")
+            {
+                var HashAl = new HashAlgorithmName("MD5");
+                using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, HashAl))
+                {
+                    return rfc2898.GetBytes(20);
+                }
+            }
+            if (choice == "SHA1")
+            {
+                var HashAl = new HashAlgorithmName("SHA1");
+                using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, HashAl))
+                {
+                    return rfc2898.GetBytes(20);
+                }
+            }
+            if (choice == "SHA256")
+            {
+                var HashAl = new HashAlgorithmName("SHA256");
+                using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, HashAl))
+                {
+                    return rfc2898.GetBytes(20);
+                }
+            }
+            if (choice == "SHA384")
+            {
+                var HashAl = new HashAlgorithmName("SHA384");
+                using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, HashAl))
+                {
+                    return rfc2898.GetBytes(20);
+                }
+            }
+            if (choice == "SHA512")
+            {
+                var HashAl = new HashAlgorithmName("SHA512");
+                using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, HashAl))
+                {
+                    return rfc2898.GetBytes(20);
+                }
+            }
+            else 
+            {
+                var HashAl = new HashAlgorithmName("SHA1");
+                using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, HashAl))
+                {
+                     return rfc2898.GetBytes(20);
+                }
+            }
+        }
+    }
+}
---
+++ PractZadan-4/PractZadan-4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace PractZadan_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string password = "V3ryC0mpl3xP455w0rd";
+            byte[] salt = Hash.GenerateSalt(32);
+            Console.WriteLine("Password : " + password);
+            Console.WriteLine("Salt = " + Convert.ToBase64String(salt));
+            Console.WriteLine();
+            var Sha256text = Hash.Sha256Hash(Encoding.UTF8.GetBytes(password));
+            Console.WriteLine("Hashed Password = " + Convert.ToBase64String(Sha256text));
+            var hashedPassword1 = Hash.HashPasswordWithSalt(Encoding.UTF8.GetBytes(password),salt);
+            Console.WriteLine("Hashed Password With Salt = " + Convert.ToBase64String(hashedPassword1));
+            Console.ReadLine();
+        }
+    }
+
+    public class Hash
+    {
+        public static byte[] GenerateSalt(int length)
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+        private static byte[] Combine(byte[] first, byte[] second)
+        {
+            var ret = new byte[first.Length + second.Length];
+            Buffer.BlockCopy(first, 0, ret, 0, first.Length);
+            Buffer.BlockCopy(second, 0, ret, first.Length, second.Length);
+            return ret;
+        }
+
+        public static byte[] Sha256Hash(byte[] toBeHashed)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(toBeHashed);
+            }
+        }
+        public static byte[] HashPasswordWithSalt(byte[] toBeHashed, byte[] salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(Combine(toBeHashed, salt));
+            }
+        }
+    }
+}
